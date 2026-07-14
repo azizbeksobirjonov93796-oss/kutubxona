f5fdd6de70064df18a6ebc2e22b6485844d8fdf2
Azizbek Sobirjonov <azizbeksobirjonov93796@gmail.com>
2026-07-14 13:01:06 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+class Kutubxona:
+    def __init__(self, nomi):
+        self.nomi = nomi
+
+    def info(self):
+        print(f"Kutubxona: {self.nomi}")
+kutubxona = Kutubxona("Yoshlar Kutubxonasi")
+print("MALUMOTLAR")
+kutubxona.info()
+print()
